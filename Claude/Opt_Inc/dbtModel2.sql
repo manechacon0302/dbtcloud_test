78502ac45aed30e62e854e8ca15497f6eece56c3
manechacon0302 <mane.chacon@propellingtech.com>
2025-10-20 11:41:46 +0200

    Improved dbt model4

---
+++ Claude/Opt_Inc/dbtModel2.sql
@@ -0,0 +1,17 @@
+{{ config(
+    materialized='table'
+) }}
+
+SELECT
+  c.name AS customer_name,
+  SUM(o.order_total) AS total_spent
+FROM
+  {{ source('dbt_semantic_layer_demo', 'fct_orders') }} AS o
+INNER JOIN
+  {{ source('dbt_semantic_layer_demo', 'dim_customers') }} AS c
+ON
+  o.customer_id = c.customer_id
+GROUP BY
+  c.name
+ORDER BY
+  total_spent DESC
